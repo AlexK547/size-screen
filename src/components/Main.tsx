b9700961747e4273ef6b6d93f2ebbbbbab95aebe
Alex Kozhenov <kozhenov1984@mail.ru>
2023-11-11 04:46:47 +0400

    add screen size

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Main() {
-  const [height, setHeight] = useState(window.innerHeight);
-  const [width, setWidth] = useState(window.innerWidth);
+  const [screenWidth, setScreenWidth] = useState(window.screen.width);
+  const [screenHeight, setScreenHeight] = useState(window.screen.height);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleSize);
+  }, []);
 
   const handleSize = () => {
-    setHeight(window.innerHeight);
-    setWidth(window.innerWidth);
-  }
+    setScreenWidth(window.screen.width);
+    setScreenHeight(window.screen.height);
+    setInnerHeight(window.innerHeight);
+    setInnerWidth(window.innerWidth);
+  };
 
   return (
     <main>
-      <h1>Размер окна</h1>
-      <div className='size__text'>Высота - <b>{height}px</b></div>
-      <div className='size__text'>Ширина - <b>{width}px</b></div>
-      <button type='button' onClick={handleSize}>Определить размер</button>
+      <div className="table-data">
+        <table>
+          <thead>
+            <tr>
+              <th>Размер</th>
+              <th>Ширина, px</th>
+              <th>Высота, px</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Размер экрана</td>
+              <td>
+                <b>{screenWidth}</b>
+              </td>
+              <td>
+                <b>{screenHeight}</b>
+              </td>
+            </tr>
+            <tr>
+              <td>Размер окна</td>
+              <td>
+                <b>{innerWidth}</b>
+              </td>
+              <td>
+                <b>{innerHeight}</b>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
